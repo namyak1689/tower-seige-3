1284ce1133ddddf7bc703f6e417c91fe6f091eeb
namyak1689 <73359382+namyak1689@users.noreply.github.com>
2022-08-18 19:25:51 -0600

    Add files via upload

---
+++ box.js
@@ -0,0 +1,14 @@
+score(){
+    if(this.visibility<0 && this.visibility >-105){
+        score++;
+    }
+}
+blocks1.score();
+blocks2.score();
+blocks3.score();
+blocks4.score();
+blocks5.score();
+blocks6.score();
+blocks7.score();
+blocks8.score();
+blocks9.score();
